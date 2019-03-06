5e3e280b3bc4db3e923ad089aceee07445c44bd9
Jochen Röse <jochen.roese@pum.de>
2019-03-06 15:47:22 +0100

    New files now get's compiled fix #2

--- commands/compile.ts
+++ commands/compile.ts
@@ -15,27 +15,32 @@ async function compileFlags(): Promise<string> {
 
 async function importFile(file: CurrentFile, flags: string, ignoreConflicts: boolean): Promise<any> {
   const api = new AtelierAPI();
+  const put = (file, flags) => {
+    api.putDoc(
+      file.name,
+      {
+        enc: false,
+        content: file.content.split(/\r?\n/)
+      },
+      true
+    )
+    .then(data => compile(file, flags))
+    .catch((error: Error) => {
+      outputChannel.appendLine(error.message);
+      outputChannel.show(true);
+      vscode.window.showErrorMessage(error.message);
+    });
+  }
+
   return api.getDoc(file.name).then(data => {
     let hasConflict = false;
     let serverDate: Date = new Date(data.result.ts);
+    
     if (serverDate>file.timestamp) {
       hasConflict = !ignoreConflicts;
     }
     if (!hasConflict) {
-      api.putDoc(
-        file.name,
-        {
-          enc: false,
-          content: file.content.split(/\r?\n/)
-        },
-        true
-      )
-      .then(data => compile(file, flags))
-      .catch((error: Error) => {
-        outputChannel.appendLine(error.message);
-        outputChannel.show(true);
-        vscode.window.showErrorMessage(error.message);
-      });
+      put(file, flags);
     } else {
       vscode.window.showErrorMessage('Server has a newer version of this document.',
       ...['Save local copy', 'Save server copy', 'cancel']).then(choice => {
@@ -51,6 +56,15 @@ async function importFile(file: CurrentFile, flags: string, ignoreConflicts: boo
           vscode.window.showErrorMessage('Compile canceled by user');
         }
       })
+    }
+  }).catch((error: Error) => {
+    if (error.message.indexOf('404')>0) {
+      // well...that's a new file...lets put it on the server
+      put(file, flags)
+    } else {
+      outputChannel.appendLine(error.message);
+      outputChannel.show(true)
+      vscode.window.showErrorMessage(error.message);
     }
   })
 }
